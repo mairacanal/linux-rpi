5bccac00804aa38605810a68936bf9f95c8104c0
Maíra Canal <mcanal@igalia.com>
2025-07-27 19:54:35 -0300

    WIP drm/v3d: Introduce Runtime Power Management
    Commit 90a64adb0876 ("drm/v3d: Get rid of pm code") removed the last
    bits of power management code that V3D had, which were actually never
    hooked. Therefore, currently, the GPU clock is enabled during probe
    and only disabled when removing the driver.
    
    Implement proper power management using the kernel's Runtime PM
    framework.
    
    Signed-off-by: Maíra Canal <mcanal@igalia.com>

--- drivers/gpu/drm/v3d/v3d_gem.c
+++ drivers/gpu/drm/v3d/v3d_gem.c
@@ -129,6 +129,9 @@ v3d_reset(struct v3d_dev *v3d)
 	DRM_DEV_ERROR(dev->dev, "Resetting GPU for hang.\n");
 	DRM_DEV_ERROR(dev->dev, "V3D_ERR_STAT: 0x%08x\n",
 		      V3D_CORE_READ(0, V3D_ERR_STAT));
+
+	v3d_pm_runtime_get(v3d);
+
 	trace_v3d_reset_begin(dev);
 
 	/* XXX: only needed for safe powerdown, not reset. */
@@ -147,6 +150,8 @@ v3d_reset(struct v3d_dev *v3d)
 	v3d_perfmon_stop(v3d, v3d->active_perfmon, false);
 
 	trace_v3d_reset_end(dev);
+
+	v3d_pm_runtime_put(v3d);
 }
 
 static void
@@ -326,7 +331,6 @@ v3d_gem_init(struct drm_device *dev)
 	struct v3d_dev *v3d = to_v3d_dev(dev);
 
 	v3d_init_hw_state(v3d);
-	v3d_mmu_set_page_table(v3d);
 }
 
 void
