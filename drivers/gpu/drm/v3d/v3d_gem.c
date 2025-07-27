23e5fb4e344589b540969865190aea58c49fe9f4
Maíra Canal <mcanal@igalia.com>
2025-07-27 19:58:07 -0300

    drm/v3d: Allocate all resources before enabling the clock
    Move all resource allocation operations before actually enabling the
    clock, as those operation don't require the GPU to be powered on.
    
    While here, use devm_reset_control_get_optional_exclusive() instead of
    open-code it.
    
    Signed-off-by: Maíra Canal <mcanal@igalia.com>

--- drivers/gpu/drm/v3d/v3d_gem.c
+++ drivers/gpu/drm/v3d/v3d_gem.c
@@ -260,7 +260,7 @@ v3d_invalidate_caches(struct v3d_dev *v3d)
 }
 
 int
-v3d_gem_init(struct drm_device *dev)
+v3d_gem_allocate(struct drm_device *dev)
 {
 	struct v3d_dev *v3d = to_v3d_dev(dev);
 	u32 pt_size = 4096 * 1024;
@@ -307,9 +307,6 @@ v3d_gem_init(struct drm_device *dev)
 		return -ENOMEM;
 	}
 
-	v3d_init_hw_state(v3d);
-	v3d_mmu_set_page_table(v3d);
-
 	v3d_gemfs_init(v3d);
 
 	ret = v3d_sched_init(v3d);
@@ -321,6 +318,15 @@ v3d_gem_init(struct drm_device *dev)
 	}
 
 	return 0;
+}
+
+void
+v3d_gem_init(struct drm_device *dev)
+{
+	struct v3d_dev *v3d = to_v3d_dev(dev);
+
+	v3d_init_hw_state(v3d);
+	v3d_mmu_set_page_table(v3d);
 }
 
 void
