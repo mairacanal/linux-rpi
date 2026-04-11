7b3eef13342626ad661fdbab6296c83177362ece
Maíra Canal <mcanal@igalia.com>
2026-04-11 15:35:30 -0300

    [BACKPORTED] drm/v3d: Use inline lock for dma fence initialization
    Since commit 1f32f310a13c ("dma-buf: inline spinlock for fence protection
    v5"), dma_fence_init() accepts a NULL external lock and will fall back to
    an inline spinlock embedded in the fence itself. The embedded spinlock
    allows the decoupling the lock and the fence lifetime.
    
    Pass NULL so each v3d fence uses its own inline lock. This will allow
    queue_lock to use spin_(un)lock() in all its uses instead of
    spin_(un)lock_irqsave().
    
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Signed-off-by: Maíra Canal <mcanal@igalia.com>

--- drivers/gpu/drm/v3d/v3d_drv.h
+++ drivers/gpu/drm/v3d/v3d_drv.h
@@ -71,6 +71,7 @@ struct v3d_queue_state {
 	/* Currently active job for this queue */
 	struct v3d_job *active_job;
 	spinlock_t queue_lock;
+	spinlock_t fence_lock;
 };
 
 /* Performance monitor object. The perform lifetime is controlled by userspace
